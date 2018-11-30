d3dee2ba2da6d3adff20de30e2c94e942c3d9ec7
Gökmen Yılmaz <gokmen1977@gmail.com>
2018-11-30 22:37:48 +0300

    Add project files.

---
+++ MainWindowViewNodel.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class MainWindowViewNodel:BindableBase
+    {
+        public  Personel Kisi { get; set; }
+
+        public DelegateCommand GuncelleCommand => new DelegateCommand(Guncelle);
+
+        private void Guncelle()
+        {
+            RaisePropertyChanged(nameof(Kisi));
+
+        
+
+            //Kisi.AdSoyadRefresh();
+
+        }
+
+        public MainWindowViewNodel()
+        {
+            var kisi = new Personel();
+
+            kisi.Ad = "gok";
+            kisi.Soyad = "faruk";
+
+            Kisi = kisi;
+        }
+    }
+}
---
+++ Personel.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class Personel : BindableBase
+    {
+        private string _ad;
+        private string _soyad;
+
+        public string Ad
+        {
+            get => _ad;
+            set
+            {
+                SetProperty(ref _ad, value);
+            }
+        }
+        public string Soyad
+        {
+            get => _soyad;
+            set
+            {
+                SetProperty(ref _soyad, value);
+            }
+        }
+
+        public string AdSoyad => Ad + " " + Soyad;
+
+        public void AdSoyadRefresh() => RaisePropertyChanged(nameof(AdSoyad));
+    }
+}
